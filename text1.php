0aed838d31cc0053c0f10c7c620ae78d76b805bd
alvinaji503 <mca503@rajagiri.edu>
2021-03-10 15:58:18 +0530

    Test

--- text1.php
+++ text1.php
@@ -10,18 +10,18 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container-fluid">
-          <a class="navbar-brand" href="text1.html">Rajagiri College</a>
+          <a class="navbar-brand" href="text1.php">Rajagiri College</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="accordion1.html">Home</a>
+                <a class="nav-link active" aria-current="page" href="accordion1.php">Home</a>
               </li>
               
               <li class="nav-item">
-                <a class="nav-link" href="text.html">Student</a>
+                <a class="nav-link" href="text.php">Student</a>
               </li>
               
             </ul>
