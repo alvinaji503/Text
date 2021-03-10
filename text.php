43d058d21990ab252c6727c3965b02ab99fd92a6
alvinaji503 <mca503@rajagiri.edu>
2021-03-10 15:54:43 +0530

    Test

--- text.php
+++ text.php
@@ -17,11 +17,11 @@
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="accordion1.html">Home</a>
+                <a class="nav-link active" aria-current="page" href="accordion1.php">Home</a>
               </li>
             
               <li class="nav-item">
-                <a class="nav-link" href="text.html">Student</a>
+                <a class="nav-link" href="text.php">Student</a>
               </li>
               
             </ul>
